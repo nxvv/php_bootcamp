6cd34d9ced9de0e88857539f50ee2ad0071c4ead
nxvv <boconewton01@gmail.com>
2022-07-29 06:03:40 +0100

    Form in php

--- php_html/functions.php
+++ php_html/functions.php
@@ -1,4 +1,5 @@
 <?php 
+
 function nav_item (string $lien, string $titre, string $linkClass = ''):  string  
 {
 
@@ -23,5 +24,27 @@ function nav_menu(string $linkClass = ''): string
     return 
         nav_item('/index.php', 'Accueil', $linkClass) .
         nav_item('/contact.php', 'Contact', $linkClass);
+}
+
+function checkbox (string $name, string $value, array $data)
+{
+    $attributes = '';
+    if(isset($data[$name]) && in_array($value, $data[$name])){
+        $attributes .= 'checked';
+    }
+    return <<<HTML
+    <input type="checkbox" name="{$name}[]" value="$value" $attributes> 
+HTML;
+}
+
+function radio (string $name, string $value, array $data)
+{
+    $attributes = '';
+    if(isset($data[$name]) && $value === $data[$name]){
+        $attributes .= 'checked';
+    }
+    return <<<HTML
+    <input type="radio" name="$name" value="$value" $attributes> 
+HTML;
 }
 ?>
